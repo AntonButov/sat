7a4c8c46cddc9748d54111072e11b567dee68460
Anton Butov <butov6101@gmail.com>
2020-04-08 17:26:12 +0300

    Хороший рабочий парсер.

--- app/src/main/java/pro/butovanton/satellite/Parser.java
+++ app/src/main/java/pro/butovanton/satellite/Parser.java
@@ -3,6 +3,8 @@ package pro.butovanton.satellite;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.XmlResourceParser;
+import android.text.TextUtils;
+import android.util.Log;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -22,127 +24,49 @@ public class Parser {
     @SuppressLint("ResourceType")
     public List parse() throws XmlPullParserException, IOException {
 
-        XmlResourceParser xmlResourceParser = context.getResources().getXml(R.xml.satellites);
+        XmlResourceParser parser = context.getResources().getXml(R.xml.satellites);
 
-        xmlResourceParser.next();
-        xmlResourceParser.nextTag();
+        List sats = new ArrayList();
+        String tmp;
 
-        return readFeed(xmlResourceParser);
-    }
-
-    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
-        List entries = new ArrayList();
-
-        parser.require(XmlPullParser.START_TAG, ns, "satellites");
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            // Starts by looking for the entry tag
-            if (name.equals("sat")) {
-                entries.add(readEntry(parser));
-            } else {
-                skip(parser);
-            }
-        }
-        return entries;
-    }
-
-    public static class Entry {
-        public final String title;
-        public final String link;
-        public final String summary;
-
-        private Entry(String title, String summary, String link) {
-            this.title = title;
-            this.summary = summary;
-            this.link = link;
-        }
-    }
-
-    // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
-// to their respective "read" methods for processing. Otherwise, skips the tag.
-    private Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
-        parser.require(XmlPullParser.START_TAG, ns, "sat name");
-        String title = null;
-        String summary = null;
-        String link = null;
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            String name = parser.getName();
-            if (name.equals("title")) {
-                title = readTitle(parser);
-            } else if (name.equals("summary")) {
-                summary = readSummary(parser);
-            } else if (name.equals("link")) {
-                link = readLink(parser);
-            } else {
-                skip(parser);
-            }
-        }
-        return new Entry(title, summary, link);
-    }
-
-    // Processes title tags in the feed.
-    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, ns, "title");
-        String title = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "title");
-        return title;
-    }
-
-    // Processes link tags in the feed.
-    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String link = "";
-        parser.require(XmlPullParser.START_TAG, ns, "link");
-        String tag = parser.getName();
-        String relType = parser.getAttributeValue(null, "rel");
-        if (tag.equals("link")) {
-            if (relType.equals("alternate")){
-                link = parser.getAttributeValue(null, "href");
-                parser.nextTag();
-            }
-        }
-        parser.require(XmlPullParser.END_TAG, ns, "link");
-        return link;
-    }
-
-    // Processes summary tags in the feed.
-    private String readSummary(XmlPullParser parser) throws IOException, XmlPullParserException {
-        parser.require(XmlPullParser.START_TAG, ns, "summary");
-        String summary = readText(parser);
-        parser.require(XmlPullParser.END_TAG, ns, "summary");
-        return summary;
-    }
-
-    // For the tags title and summary, extracts their text values.
-    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
-        String result = "";
-        if (parser.next() == XmlPullParser.TEXT) {
-            result = parser.getText();
-            parser.nextTag();
-        }
-        return result;
-    }
-
-    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-        if (parser.getEventType() != XmlPullParser.START_TAG) {
-            throw new IllegalStateException();
-        }
-        int depth = 1;
-        while (depth != 0) {
-            switch (parser.next()) {
-                case XmlPullParser.END_TAG:
-                    depth--;
+        while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+            switch (parser.getEventType()) {
+                // начало документа
+                case XmlPullParser.START_DOCUMENT:
+                    Log.d("DEBUG", "START_DOCUMENT");
                     break;
+                // начало тэга
                 case XmlPullParser.START_TAG:
-                    depth++;
+                    Log.d("DEBUG", "START_TAG: name = " + parser.getName()
+                            + ", depth = " + parser.getDepth() + ", attrCount = "
+                            + parser.getAttributeCount());
+                    tmp = "";
+                    for (int i = 0; i < parser.getAttributeCount(); i++) {
+                        tmp = tmp + parser.getAttributeName(6) + " = "
+                                + parser.getAttributeValue(i) + ", ";
+                    }
+                    if (!TextUtils.isEmpty(tmp))
+                        Log.d("DEBUG", "Attributes: " + tmp);
+                    break;
+                // конец тэга
+                case XmlPullParser.END_TAG:
+                    Log.d("DEBUG", "END_TAG: name = " + parser.getName());
+                    break;
+                // содержимое тэга
+
+                default:
                     break;
             }
+            // следующий элемент
+            parser.next();
         }
+        Log.d("DEBUG", "END_DOCUMENT");
+
+        return sats;
+    }
+
+    sat reeadSat (){
+        return new sat();
     }
 }
 
